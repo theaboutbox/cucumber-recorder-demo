37e0fccd1c79be505fb5d04c222e599562195e00
Cameron Pope <cameron@theaboutbox.com>
2012-04-25 15:45:40 -0600

    Bare rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CucumberRecorderDemo::Application.config.secret_token = '0aeaab8f7a4af08ae1297669b0ea1f9918ef00fb46e3aa6266bd29a93334e7fd31c3c8626fe103cfcfc14160b9d492766cc3504b6da5b71bf03a69fa5c5f4b6a'
